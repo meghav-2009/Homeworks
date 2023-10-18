e2977349cb5d854cc5ab1f6d79fb76542adfe82a
Megha Veeregowda <72877116+meghav-2009@users.noreply.github.com>
2023-10-18 00:36:18 -0600

    Add files via upload

---
+++ SQL_Analysis.sql
@@ -0,0 +1,74 @@
+--How many animals of each type have outcomes?
+SELECT animal_type, COUNT(DISTINCT animal_id) as number_of_animals
+FROM animaldimension
+GROUP BY animal_type;
+
+--How many animals are there with more than 1 outcome?
+SELECT COUNT(animalkey) AS animals_with_more_than_one_outcome
+FROM (
+    SELECT animalkey
+    FROM animalfact
+    GROUP BY animalkey
+    HAVING COUNT(DISTINCT outcomekey) > 1
+) AS animals_with_multiple_outcomes;
+
+--What are the top 5 months for outcomes? 
+SELECT month, COUNT(*) as outcome_count
+FROM timedimension
+JOIN animalfact ON timedimension.timekey = animalfact.timekey
+GROUP BY month
+ORDER BY outcome_count DESC
+LIMIT 5;
+
+--What is the total number of kittens, adults, and seniors, whose outcome is "Adopted"?
+SELECT 
+    animal_type,
+    CASE 
+        WHEN age = 'Kitten' THEN 'Kitten'
+        WHEN age = 'Senior cat' THEN 'Senior'
+        ELSE 'Adult'
+    END AS age_group,
+    COUNT(*) AS total
+FROM animalfact af
+JOIN animaldimension ad ON af.animalkey = ad.animalkey
+JOIN outcomedimension od ON af.outcomekey = od.outcomekey
+WHERE outcome_type = 'Adopted'
+    AND animal_type = 'Cat'
+GROUP BY animal_type, age_group;
+
+--Conversely, among all the cats who were "Adopted", what is the total number of kittens, adults, and seniors?
+SELECT 
+    CASE 
+        WHEN age = 'Kitten' THEN 'Kitten'
+        WHEN age = 'Senior cat' THEN 'Senior'
+        ELSE 'Adult'
+    END AS age_group,
+    COUNT(*) AS total
+FROM animalfact af
+JOIN animaldimension ad ON af.animalkey = ad.animalkey
+JOIN outcomedimension od ON af.outcomekey = od.outcomekey
+WHERE outcome_type = 'Adopted'
+    AND animal_type = 'Cat'
+GROUP BY age_group;
+
+
+--For each date, what is the cumulative total of outcomes up to and including this date?
+SELECT
+    ts AS date,
+    SUM(COUNT(*)) OVER (ORDER BY ts) AS cumulative_total
+FROM
+    timedimension
+LEFT JOIN
+    animalfact ON timedimension.timekey = animalfact.timekey
+GROUP BY
+    ts
+ORDER BY
+    ts;
+
+
+
+
+
+
+
+
